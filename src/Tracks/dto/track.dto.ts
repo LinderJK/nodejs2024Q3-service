2aeffec1be9ca859d5f0da1585f071ea5c0defa9
LinderJK <slepchenkovia@gmail.com>
2024-11-09 18:37:28 +0300

    fix: change track dto to optional some fields

--- src/Tracks/dto/track.dto.ts
+++ src/Tracks/dto/track.dto.ts
@@ -1,12 +1,14 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateTrackDto {
   @IsString()
   name: string;
 
+  @IsOptional()
   @IsString()
   artistId: string | null;
 
+  @IsOptional()
   @IsString()
   albumId: string | null;
 
