79be715d4df46f162d894e6c62739b7ee2706845
LinderJK <slepchenkovia@gmail.com>
2024-11-09 16:33:12 +0300

    feat: add port and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 bootstrap();
