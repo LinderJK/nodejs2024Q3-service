a2eb112cdb2a29c6b5ef17bc1cb26312fa46b29e
LinderJK <slepchenkovia@gmail.com>
2024-11-10 16:18:40 +0300

    feat: add async for methods

--- src/Favorites/favorites.controller.ts
+++ src/Favorites/favorites.controller.ts
@@ -15,40 +15,50 @@ export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
   @Get()
-  getFavorites() {
+  async getFavorites() {
     return this.favoritesService.getFavorites();
   }
 
   @Post('track/:id')
-  addTrackToFavorites(@Param('id', new ParseUUIDPipe()) trackId: string) {
+  async addTrackToFavorites(@Param('id', new ParseUUIDPipe()) trackId: string) {
     return this.favoritesService.addTrackToFavorites(trackId);
   }
 
   @Delete('track/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  removeTrackFromFavorites(@Param('id', new ParseUUIDPipe()) trackId: string) {
+  async removeTrackFromFavorites(
+    @Param(
+      'id',
+      new ParseUUIDPipe({ errorHttpStatusCode: HttpStatus.BAD_REQUEST }),
+    )
+    trackId: string,
+  ) {
     this.favoritesService.removeTrackFromFavorites(trackId);
   }
 
   @Post('album/:id')
-  addAlbumToFavorites(@Param('id', new ParseUUIDPipe()) albumId: string) {
+  async addAlbumToFavorites(@Param('id', new ParseUUIDPipe()) albumId: string) {
     return this.favoritesService.addAlbumToFavorites(albumId);
   }
 
   @Delete('album/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  removeAlbumFromFavorites(@Param('id', new ParseUUIDPipe()) albumId: string) {
+  async removeAlbumFromFavorites(
+    @Param('id', new ParseUUIDPipe()) albumId: string,
+  ) {
     this.favoritesService.removeAlbumFromFavorites(albumId);
   }
 
   @Post('artist/:id')
-  addArtistToFavorites(@Param('id', new ParseUUIDPipe()) artistId: string) {
+  async addArtistToFavorites(
+    @Param('id', new ParseUUIDPipe()) artistId: string,
+  ) {
     return this.favoritesService.addArtistToFavorites(artistId);
   }
 
   @Delete('artist/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  removeArtistFromFavorites(
+  async removeArtistFromFavorites(
     @Param('id', new ParseUUIDPipe()) artistId: string,
   ) {
     this.favoritesService.removeArtistFromFavorites(artistId);
