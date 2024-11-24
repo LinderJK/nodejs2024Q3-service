135014aa3bcaacfce0dfe8d7dec846ea9b8778f7
LinderJK <slepchenkovia@gmail.com>
2024-11-24 17:14:44 +0300

    feat: add logging service

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AuthGuard } from './Auth/auth.guard';
 import { JwtService } from '@nestjs/jwt';
+import { LoggingService } from './LoggingService/logging.service';
+import { CustomExceptionFilter } from './LoggingService/exception.filter';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+  const loggingService = app.get(LoggingService);
+
+  process.on('uncaughtException', (error) => {
+    loggingService.error('Uncaught Exception', error.stack);
+  });
+
+  process.on('unhandledRejection', (reason) => {
+    loggingService.error('Unhandled Rejection', JSON.stringify(reason));
+  });
+
   const config = new DocumentBuilder()
     .setTitle('REST Service')
     .setDescription(
@@ -17,10 +29,15 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('library')
     .build();
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalGuards(new AuthGuard(new JwtService()));
+
+  app.useGlobalFilters(new CustomExceptionFilter(loggingService));
+
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 bootstrap();
